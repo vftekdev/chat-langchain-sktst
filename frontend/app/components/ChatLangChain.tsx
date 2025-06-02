46d0dbefb0f1d0dd89b06b41dc91ec37d11f7c81
vfzale <zcham@verafiles.org>
2025-06-02 15:51:28 +0800

    test alpha push

--- frontend/app/components/ChatLangChain.tsx
+++ frontend/app/components/ChatLangChain.tsx
@@ -121,7 +121,7 @@ function ChatLangChainComponent({test} : {test:any}): React.ReactElement {
   const userlength = test.length;
   const truncatetext = userlength > 10 ? test.substring(0,10) + "..." : test;
   return (
-    <div className="overflow-hidden w-full flex lg:flex-row flex-col relative bg-white">
+    <div className="overflow-hidden w-full flex lg:flex-row flex-col relative bg-white dark:bg-black">
       <div className="flex absolute top-2 lg:top-5 right-60 z-10 mx-2 my-4 border border-[#D7D7D7] rounded-lg px-2">
         <a className="text-black" href="https://verafiles.atlassian.net/jira/software/c/form/03737abd-4aaf-4e2c-960c-570ac7f11f12" target="_blank">Report Issue</a>
       </div>
@@ -139,6 +139,19 @@ function ChatLangChainComponent({test} : {test:any}): React.ReactElement {
         <form action={SignOut}>
             <button>Sign Out</button>
         </form>
+      </div>
+      <div className="absolute bottom-2 right-2 lg:bottom-5 lg:right-5 z-10">
+        <button onClick={() => document.body.classList.toggle('dark')}
+          className="h-12 w-12 rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+          <svg className="fill-violet-700 block dark:hidden" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+          </svg>
+          <svg className="fill-yellow-500 hidden dark:block" fill="currentColor" viewBox="0 0 20 20">
+              <path
+                  d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                  fill-rule="evenodd" clip-rule="evenodd"></path>
+          </svg>
+        </button>
       </div>
       <div className="">
         <ThreadHistory />
