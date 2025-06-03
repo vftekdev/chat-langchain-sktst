38d287ca95cb34a5a8c83a6d7327cf733a9b7753
VERA Files Tech Team <dev@verafiles.org>
2025-06-03 10:58:45 +0800

    used the old format in prompts, deleted a space  after general prompt name

--- backend/retrieval_graph/prompts.py
+++ backend/retrieval_graph/prompts.py
@@ -6,8 +6,8 @@ client = Client()
 # fetch from langsmith
 ROUTER_SYSTEM_PROMPT = (
     client.pull_prompt("verafiles/seek-search-assistant_router-prompt")
-    #.messages[0]
-    #.prompt.template
+    .messages[0]
+    .prompt.template
 )
 GENERATE_QUERIES_SYSTEM_PROMPT = (
     client.pull_prompt("langchain-ai/chat-langchain-generate-queries-prompt")
@@ -16,21 +16,21 @@ GENERATE_QUERIES_SYSTEM_PROMPT = (
 )
 MORE_INFO_SYSTEM_PROMPT = (
     client.pull_prompt("verafiles/seek-search-assistant_more_info_prompt")
-    #.messages[0]
-    #.prompt.template
+    .messages[0]
+    .prompt.template
 )
 RESEARCH_PLAN_SYSTEM_PROMPT = (
     client.pull_prompt("verafiles/seek-search-assistant_research_plan_prompt")
-    #.messages[0]
-    #.prompt.template
+    .messages[0]
+    .prompt.template
 )
 GENERAL_SYSTEM_PROMPT = (
-    client.pull_prompt("verafiles/seek-search-assistant_general_prompt ")
-    #.messages[0]
-    #.prompt.template
+    client.pull_prompt("verafiles/seek-search-assistant_general_prompt")
+    .messages[0]
+    .prompt.template
 )
 RESPONSE_SYSTEM_PROMPT = (
     client.pull_prompt("verafiles/seek_search_assistant-response_prompt")
-    #.messages[0]
-    #.prompt.template
+    .messages[0]
+    .prompt.template
 )
