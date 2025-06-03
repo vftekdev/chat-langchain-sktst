154755e5092277e2d3f24c5bed1262679cde4489
vfzale <zcham@verafiles.org>
2025-06-03 14:52:06 +0800

    testing responsiveness v2

--- frontend/app/components/chat-interface/index.tsx
+++ frontend/app/components/chat-interface/index.tsx
@@ -98,7 +98,7 @@ export const ThreadChat: FC<ThreadChatProps> = (props: ThreadChatProps) => {
             messages={props.messages}
           />
           <div className="flex">
-            <p className="w-full lg:ml-24 md:ml-6">
+            <p className="w-full lg:ml-24 md:ml-6 ml-3">
               SEEK uses VERA Files articles to create answers using AI. 
               AI answers may contain errors. 
               Always check the sources cited to verify the facts.
