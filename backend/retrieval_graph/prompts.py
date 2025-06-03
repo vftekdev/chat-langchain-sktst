ce3835ca17c10d98e68e86745cdf1d30132cd0d9
VERA Files Tech Team <dev@verafiles.org>
2025-06-03 10:35:13 +0800

    added author handles in prompts

--- backend/retrieval_graph/prompts.py
+++ backend/retrieval_graph/prompts.py
@@ -5,9 +5,9 @@ from langsmith import Client
 client = Client()
 # fetch from langsmith
 ROUTER_SYSTEM_PROMPT = (
-    client.pull_prompt("seek-search-assistant_router-prompt")
-    .messages[0]
-    .prompt.template
+    client.pull_prompt("verafiles/seek-search-assistant_router-prompt")
+    #.messages[0]
+    #.prompt.template
 )
 GENERATE_QUERIES_SYSTEM_PROMPT = (
     client.pull_prompt("langchain-ai/chat-langchain-generate-queries-prompt")
@@ -15,22 +15,22 @@ GENERATE_QUERIES_SYSTEM_PROMPT = (
     .prompt.template
 )
 MORE_INFO_SYSTEM_PROMPT = (
-    client.pull_prompt("seek-search-assistant_more_info_prompt")
-    .messages[0]
-    .prompt.template
+    client.pull_prompt("verafiles/seek-search-assistant_more_info_prompt")
+    #.messages[0]
+    #.prompt.template
 )
 RESEARCH_PLAN_SYSTEM_PROMPT = (
-    client.pull_prompt("seek-search-assistant_research_plan_prompt")
-    .messages[0]
-    .prompt.template
+    client.pull_prompt("verafiles/seek-search-assistant_research_plan_prompt")
+    #.messages[0]
+    #.prompt.template
 )
 GENERAL_SYSTEM_PROMPT = (
-    client.pull_prompt("seek-search-assistant_general_prompt ")
-    .messages[0]
-    .prompt.template
+    client.pull_prompt("verafiles/seek-search-assistant_general_prompt ")
+    #.messages[0]
+    #.prompt.template
 )
 RESPONSE_SYSTEM_PROMPT = (
-    client.pull_prompt("seek_search_assistant-response_prompt")
-    .messages[0]
-    .prompt.template
+    client.pull_prompt("verafiles/seek_search_assistant-response_prompt")
+    #.messages[0]
+    #.prompt.template
 )
