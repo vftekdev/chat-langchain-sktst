b0ef747287693cd384e659d37c762d67d7c1f56a
vfzale <zcham@verafiles.org>
2025-06-17 18:00:56 +0800

    font color change

--- frontend/app/components/SuggestedQuestions.tsx
+++ frontend/app/components/SuggestedQuestions.tsx
@@ -26,7 +26,7 @@ export function SuggestedQuestions() {
           key={`suggested-question-${idx}`}
           className="flex w-full bg-[#F7F9FB] rounded-lg cursor-pointer col-span-12 xl:col-span-4"
         >
-          <p className="flex flex-row gap-2 px-4 py-1.5 text-[#0F5579] text-lg transition-colors ease-in hover:text-[#2891E0]">
+          <p className="flex flex-row transition-colors ease-in items-center font-light text-[#4B4B4B] text-wrap text-lg hover:text-black gap-2 px-4 py-1">
             {question}
           </p>
           <NextImage
