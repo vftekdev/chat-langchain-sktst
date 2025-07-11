203417ae2a156df874990c2fcc8462a2fec0b6e8
VERA Files Tech Team <dev@verafiles.org>
2025-07-11 18:31:00 +0800

    Latest , Recent retrievals using post_date filter in Weaviate

--- backend/retrieval_graph/graph.py
+++ backend/retrieval_graph/graph.py
@@ -149,21 +149,21 @@ async def create_research_plan(
     configuration = AgentConfiguration.from_runnable_config(config)
     model = load_chat_model(configuration.query_model).with_structured_output(Plan)
 
-    pattern = r"latest|recent|current"
-    user_query = (state.messages[-1].content).lower()
+    # pattern = r"latest|recent|current"
+    # user_query = (state.messages[-1].content).lower()
 
-    match = re.search(pattern, user_query)
-    if match:
-        today = datetime.now().date()
+    # match = re.search(pattern, user_query)
+    # if match:
+    #    today = datetime.now().date()
         # last_month = (today.replace(day=1) - timedelta(days=1)).strftime("%B %Y")
         # last_month = (today.replace(day=1) - timedelta(days=1)).strftime("%Y-%m")
-        last_month = (today.replace(month=today.month-1))
+    #    last_month = (today.replace(month=today.month-1))
         # current_month = today.strftime("%B %Y")
-        current_month = today.strftime("%Y-%m")
-        current_year = today.strftime("%Y")
+    #    current_month = today.strftime("%Y-%m")
+    #    current_year = today.strftime("%Y")
 
         # state.messages[-1].content = f"{state.messages[-1].content} When possible, focus on articles published in {current_month}, or in {last_month}, or in {current_year}."
-        state.messages[-1].content = f"{state.messages[-1].content} When possible, focus on articles posted from {last_month} until {today}."
+    #    state.messages[-1].content = f"{state.messages[-1].content} When possible, focus on articles posted from {last_month} until {today}."
 
     # print("STATE MESSAGES: ", state.messages)
 
@@ -241,11 +241,15 @@ async def respond(
     model = load_chat_model(configuration.response_model)
 
     # re-rank documents
+    #if ( re.search(r"latest|recent|current", state.query.lower())):
+    #    top_k = 6
+    #    state.documents = state.documents[:top_k]
+    #else:
     compressor = VoyageAIRerank(
         model="rerank-2-lite", voyageai_api_key=os.environ["VOYAGE_API_KEY"], top_k=6
     )
     state.documents = compressor.compress_documents(state.documents, state.query)
-
+    
     # top_k = 20
     # context = format_docs(state.documents[:top_k])
     context = format_docs(state.documents)
