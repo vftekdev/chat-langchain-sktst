8fcf9bb90212612de9877bd983c18fd6e3c76ed1
VERA Files Tech Team <dev@verafiles.org>
2025-07-15 19:05:52 +0800

    Voyage 3.5 lite embedding, 3k chunk size

--- backend/constants.py
+++ backend/constants.py
@@ -1,6 +1,6 @@
 # WEAVIATE_DOCS_INDEX_NAME = "wy_test_OpenAI_text_embedding_3_small"
 # WEAVIATE_DOCS_INDEX_NAME = "wy_test_OpenAI_text_embedding_3_small_4000"
-# WEAVIATE_DOCS_INDEX_NAME = "wy_test_VoyageAI_voyage_3_lite"
+WEAVIATE_DOCS_INDEX_NAME = "test_voyage_35_lite_3k"
 # WEAVIATE_DOCS_INDEX_NAME = "wy_test_VoyageAI_voyage_law_2"
 
 # WEAVIATE_DOCS_INDEX_NAME = "vfseek_openai_6k_embedding"
@@ -8,4 +8,5 @@
 
 # WEAVIATE_DOCS_INDEX_NAME = "vfseek4k_openai_beta"
 # WEAVIATE_DOCS_INDEX_NAME = "chatvfsik"
-WEAVIATE_DOCS_INDEX_NAME = "Vfseek_all_4k_openai_beta"
+# WEAVIATE_DOCS_INDEX_NAME = "Vfseek_all_4k_openai_beta"
+# WEAVIATE_DOCS_INDEX_NAME = "vfseek_openai_embedding_3small_3k"
