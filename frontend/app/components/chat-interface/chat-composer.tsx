e6a85b138fc81f807d88fc87aa3266b19d616305
vfzale <zcham@verafiles.org>
2025-07-17 12:44:28 +0800

    radio button

--- frontend/app/components/chat-interface/chat-composer.tsx
+++ frontend/app/components/chat-interface/chat-composer.tsx
@@ -7,6 +7,7 @@ import { SendHorizontalIcon } from "lucide-react";
 import { BaseMessage } from "@langchain/core/messages";
 import { TooltipIconButton } from "../ui/assistant-ui/tooltip-icon-button";
 import { cn } from "@/app/utils/cn";
+import { useState } from "react";
 
 export interface ChatComposerProps {
   messages: BaseMessage[];
@@ -31,46 +32,58 @@ export const ChatComposer: FC<ChatComposerProps> = (
   props: ChatComposerProps,
 ) => {
   const isEmpty = props.messages.length === 0;
-
+  const [selectedValue, setSelectedValue] = useState('');
   return (
     <ComposerPrimitive.Root
       className={cn(
         // "bg-[#282828] focus-within:border-aui-ring/20 flex w-full items-center md:justify-left justify-center rounded-lg border px-2.5 py-2.5 shadow-sm transition-all duration-300 ease-in-out border-gray-600"
-        "focus-within:border-aui-ring/20 flex w-full items-center md:justify-left justify-center rounded-2xl px-2 py-2 shadow-lg transition-all duration-300 ease-in-out border-[#A5A5A5] bg-[#F7F9FB]",
+        "focus-within:border-aui-ring/20 flex flex-col w-full md:justify-left justify-center rounded-2xl px-2 py-2 shadow-lg transition-all duration-300 ease-in-out border-[#A5A5A5] bg-[#F7F9FB]",
         isEmpty ? "" : "lg:ml-24 md:ml-6 ml-3",
         isEmpty ? "w-full" : "md:w-[85%] w-[95%] md:max-w-[832px]",
       )}
     >
-      <ComposerPrimitive.Input
-        autoFocus
-        placeholder="Ask a question"
-        rows={1}
-        maxLength={480}
-        className="placeholder:text-[#8D8D8D] font-helveticaneuelight text-black max-h-40 flex-1 resize-none border-none bg-transparent px-4 py-3 text-sm md:text-xl outline-none focus:ring-0 disabled:cursor-not-allowed"
-      />
-      <div className="flex-shrink-0">
-        <ThreadPrimitive.If running={false} disabled={props.submitDisabled}>
-          <ComposerPrimitive.Send asChild>
-            <TooltipIconButton
-              tooltip="Send"
-              variant="default"
-              className="mr-2 my-1 size-8 p-2 bg-transparent text-black shadow-none transition-opacity ease-in"
-            >
-              <SendHorizontalIcon />
-            </TooltipIconButton>
-          </ComposerPrimitive.Send>
-        </ThreadPrimitive.If>
-        <ThreadPrimitive.If running>
-          <ComposerPrimitive.Cancel asChild>
-            <TooltipIconButton
-              tooltip="Cancel"
-              variant="default"
-              className="my-1 size-8 p-2 transition-opacity ease-in"
-            >
-              <CircleStopIcon />
-            </TooltipIconButton>
-          </ComposerPrimitive.Cancel>
-        </ThreadPrimitive.If>
+      <div className="flex w-full">
+        <ComposerPrimitive.Input
+          autoFocus
+          placeholder="Ask a question"
+          rows={1}
+          maxLength={480}
+          className="placeholder:text-[#8D8D8D] font-helveticaneuelight text-black max-h-40 flex-1 resize-none border-none bg-transparent px-4 py-3 text-sm md:text-xl outline-none focus:ring-0 disabled:cursor-not-allowed"
+        />
+        <div className="flex-shrink-0">
+          <ThreadPrimitive.If running={false} disabled={props.submitDisabled}>
+            <ComposerPrimitive.Send asChild>
+              <TooltipIconButton
+                tooltip="Send"
+                variant="default"
+                className="mr-2 my-1 size-8 p-2 bg-transparent text-black shadow-none transition-opacity ease-in"
+              >
+                <SendHorizontalIcon />
+              </TooltipIconButton>
+            </ComposerPrimitive.Send>
+          </ThreadPrimitive.If>
+          <ThreadPrimitive.If running>
+            <ComposerPrimitive.Cancel asChild>
+              <TooltipIconButton
+                tooltip="Cancel"
+                variant="default"
+                className="my-1 size-8 p-2 transition-opacity ease-in"
+              >
+                <CircleStopIcon />
+              </TooltipIconButton>
+            </ComposerPrimitive.Cancel>
+          </ThreadPrimitive.If>
+        </div>
+      </div>
+      <div className="flex gap-2">
+        <div>
+          <input className="" type="radio" name="response_type_radio" value="simple" id="response_type_radio_1"  onChange={(e) => setSelectedValue(e.target.value)} checked />
+          <label htmlFor="response_type_radio_1">Simple</label>
+        </div>
+        <div>
+          <input className="" type="radio" name="response_type_radio" value="complex"  onChange={(e) => setSelectedValue(e.target.value)} id="response_type_radio_2" />
+          <label htmlFor="response_type_radio_2">Complex</label>
+        </div>
       </div>
     </ComposerPrimitive.Root>
   );
