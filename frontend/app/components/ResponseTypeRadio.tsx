1875a92f087ed35a78d8a73a797e4f88f1d01187
vfzale <zcham@verafiles.org>
2025-07-22 14:36:31 +0800

    ui change to QA mode

--- frontend/app/components/ResponseTypeRadio.tsx
+++ frontend/app/components/ResponseTypeRadio.tsx
@@ -8,10 +8,11 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./ui/select";
+<div>test123</div>
 
 const responseTypesAndLabels: Partial<Record<ResponseType, string>> = {
-  "simple": "Simple",
-  "complex": "Complex",
+  "simple": "Quick Response (3-10 secs)",
+  "complex": "Think Deeper (20-30 secs)",
 };
 
 export function ResponseTypeComponent() {
@@ -24,12 +25,12 @@ export function ResponseTypeComponent() {
       value={selectedResponseType}
       defaultValue="anthropic/claude-3-7-sonnet-20250219"
     >
-      <SelectTrigger className="w-[180px] border-gray-600 text-gray-200 bg-yellow-500">
-        <SelectValue placeholder="Model" />
+      <SelectTrigger className="w-[240px] text-teal-600">
+        <SelectValue placeholder="Quick Response" />
       </SelectTrigger>
-      <SelectContent className="bg-[#282828] text-gray-200 border-gray-600">
+      <SelectContent className="text-teal-600">
         {Object.entries(responseTypesAndLabels).map(([responseType, label]) => (
-          <SelectItem className="hover:bg-[#2b2b2b]" key={responseType} value={responseType}>
+          <SelectItem className="!hover:bg-[#2b2b2b] !hover:text-teal-400" key={responseType} value={responseType}>
             {label}
           </SelectItem>
         ))}
