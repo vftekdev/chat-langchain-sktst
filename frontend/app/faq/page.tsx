7cdad7a4481cdc2772a70e76e43ca3636111d024
vfzale <zcham@verafiles.org>
2025-07-22 14:46:02 +0800

    toc, privacy, and faq page

---
+++ frontend/app/faq/page.tsx
@@ -0,0 +1,7 @@
+export default function faq() {
+    return(
+        <main>
+            FAQ
+        </main>
+    )
+}
---
+++ frontend/app/toc/page.tsx
@@ -0,0 +1,7 @@
+export default function toc() {
+    return(
+        <main>
+            toc
+        </main>
+    )
+}
