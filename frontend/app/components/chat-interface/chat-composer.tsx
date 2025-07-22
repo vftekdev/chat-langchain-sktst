b98dc37a279ce0b4226a19a0dace39a00180c042
vfzale <zcham@verafiles.org>
2025-07-22 15:16:52 +0800

    more padding and margin changes

--- frontend/app/components/chat-interface/chat-composer.tsx
+++ frontend/app/components/chat-interface/chat-composer.tsx
@@ -43,7 +43,7 @@ export const ChatComposer: FC<ChatComposerProps> = (
     <ComposerPrimitive.Root
       className={cn(
         // "bg-[#282828] focus-within:border-aui-ring/20 flex w-full items-center md:justify-left justify-center rounded-lg border px-2.5 py-2.5 shadow-sm transition-all duration-300 ease-in-out border-gray-600"
-        "focus-within:border-aui-ring/20 flex flex-col w-full md:justify-left justify-center rounded-2xl px-2 py-2 shadow-lg transition-all duration-300 ease-in-out border-[#A5A5A5] bg-[#F7F9FB]",
+        "focus-within:border-aui-ring/20 flex flex-col w-full md:justify-left justify-center rounded-2xl px-2 py-3 shadow-lg transition-all duration-300 ease-in-out border-[#A5A5A5] bg-[#F7F9FB]",
         isEmpty ? "" : "lg:ml-24 md:ml-6 ml-3",
         isEmpty ? "w-full" : "md:w-[85%] w-[95%] md:max-w-[832px]",
       )}
@@ -54,7 +54,7 @@ export const ChatComposer: FC<ChatComposerProps> = (
           placeholder="Ask a question"
           rows={1}
           maxLength={480}
-          className="placeholder:text-[#8D8D8D] font-helveticaneuelight text-black max-h-40 flex-1 resize-none border-none bg-transparent px-4 py-3 text-sm md:text-xl outline-none focus:ring-0 disabled:cursor-not-allowed"
+          className="placeholder:text-[#8D8D8D] font-helveticaneuelight text-black max-h-40 flex-1 resize-none border-none bg-transparent px-4 py-2 text-sm md:text-xl outline-none focus:ring-0 disabled:cursor-not-allowed"
         />
         <div className="flex-shrink-0">
           <ThreadPrimitive.If running={false} disabled={props.submitDisabled}>
