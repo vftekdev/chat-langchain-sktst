5d45d74d704df2805eb9134cfbebfc8388ee058d
vfzale <zcham@verafiles.org>
2025-07-24 12:09:15 +0800

    updated QA mode

--- frontend/app/components/ResponseTypeRadio.tsx
+++ frontend/app/components/ResponseTypeRadio.tsx
@@ -8,11 +8,15 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./ui/select";
-<div>test123</div>
 
 const responseTypesAndLabels: Partial<Record<ResponseType, string>> = {
   "simple": "Quick Response (3-10 secs)",
   "complex": "Think Deeper (20-30 secs)",
+};
+
+const responseTypesAndLabels1: Partial<Record<string, string>> = {
+  "simple": "Best for everyday conversation",
+  "complex": "Better for more complex topics",
 };
 
 export function ResponseTypeComponent() {
@@ -25,13 +29,18 @@ export function ResponseTypeComponent() {
       value={selectedResponseType}
       defaultValue="anthropic/claude-3-7-sonnet-20250219"
     >
-      <SelectTrigger className="w-[240px] text-teal-600 mx-2">
+      <SelectTrigger className="w-[250px] text-teal-600 mx-2">
         <SelectValue placeholder="Quick Response" />
       </SelectTrigger>
       <SelectContent className="text-teal-600">
         {Object.entries(responseTypesAndLabels).map(([responseType, label]) => (
-          <SelectItem className="!hover:bg-[#2b2b2b] !hover:text-teal-400" key={responseType} value={responseType}>
+          <SelectItem className="" key={responseType} value={responseType}>
+            <p className="m-0 tracking-wide">
             {label}
+            </p>
+            <p className="text-xs font-extralight text-gray-900 m-0 tracking-widest">
+              {responseTypesAndLabels1[responseType]}
+            </p>
           </SelectItem>
         ))}
       </SelectContent>
