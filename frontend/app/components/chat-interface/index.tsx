28b6f8298613730703a08aa3c0a0f93b4dc28f07
VERA Files Tech Team <dev@verafiles.org>
2025-07-28 01:12:48 +0800

    Revert "icon changes"
    This reverts commit 687a9aef75ae7afdeeb9c1b1c537cb0afb38a98c.

--- frontend/app/components/chat-interface/index.tsx
+++ frontend/app/components/chat-interface/index.tsx
@@ -106,7 +106,7 @@ export const ThreadChat: FC<ThreadChatProps> = (props: ThreadChatProps) => {
               SEEK uses VERA Files articles to create answers using AI. 
               AI answers may contain errors. 
               Always check the sources cited to verify the facts.
-              <a className="text-blue hover:border-0" href="https://verafiles.atlassian.net/jira/software/c/form/03737abd-4aaf-4e2c-960c-570ac7f11f12" target="_blank"> Report Issues.</a>
+              <a className="text-blue" href="https://verafiles.atlassian.net/jira/software/c/form/03737abd-4aaf-4e2c-960c-570ac7f11f12" target="_blank"> Report Issues.</a>
             </p>
           </div>
         </div>
