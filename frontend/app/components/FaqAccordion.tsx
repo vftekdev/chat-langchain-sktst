dd1c1520cfd48ae0f69474d24a4e0508e1751720
vfzale <zcham@verafiles.org>
2025-07-28 19:35:15 +0800

    faq changes

--- frontend/app/components/FaqAccordion.tsx
+++ frontend/app/components/FaqAccordion.tsx
@@ -3,15 +3,15 @@ import * as Accordion from "@radix-ui/react-accordion";
 
 const AccordionComponent = ({title, desc} : {title:any, desc:any}) => {
     return (
-        <div className="flex flex-col bg-white rounded-lg shadow-lg mt-2">
+        <div className="flex flex-col bg-white rounded-lg shadow-lg mt-4">
             <Accordion.Root type="multiple">
                 <Accordion.Item value="item-1">
-                    <Accordion.Trigger className="flex px-12 py-6 max-w-[400px] md:min-w-[700px]">
-                        <div className="self-start font-helveticaneuemedium">
+                    <Accordion.Trigger className="flex px-12 py-6 max-w-[400px] md:min-w-[700px] hover:text-[#006C68]">
+                        <div className="self-start font-helveticaneue">
                         {title}
                         </div>
                     </Accordion.Trigger>
-                    <Accordion.Content className="px-12 pb-6 max-w-[700px] font-helveticaneuelight">
+                    <Accordion.Content className="px-12 pb-6 max-w-[700px]">
                         <p style={{whiteSpace:'pre-line'}}>
                         {desc}
                         </p>
