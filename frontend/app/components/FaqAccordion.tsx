bbd46e475bee919b9e594b1c2a86e996580f2923
vfzale <zcham@verafiles.org>
2025-07-28 20:46:43 +0800

    more hover changes on faq and removed underline in back to home

--- frontend/app/components/FaqAccordion.tsx
+++ frontend/app/components/FaqAccordion.tsx
@@ -6,7 +6,7 @@ const AccordionComponent = ({title, desc} : {title:any, desc:any}) => {
         <div className="flex flex-col bg-white rounded-lg shadow-lg mt-4">
             <Accordion.Root type="multiple">
                 <Accordion.Item value="item-1">
-                    <Accordion.Trigger className="flex px-12 py-6 max-w-[400px] md:min-w-[700px] hover:text-[#006C68]">
+                    <Accordion.Trigger className="flex px-12 py-6 max-w-[400px] md:min-w-[700px] hover:bg-[#006C68] hover:rounded-lg hover:text-white data-[state=open]:text-[#006C68]">
                         <div className="self-start font-helveticaneue">
                         {title}
                         </div>
