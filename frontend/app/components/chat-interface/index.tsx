5f25222af4da1fb865cb111c3c7ecbcd170b86fe
vfzale <zcham@verafiles.org>
2025-11-11 15:10:42 +0800

    more dark mode tests

--- frontend/app/components/chat-interface/index.tsx
+++ frontend/app/components/chat-interface/index.tsx
@@ -109,7 +109,7 @@ export const ThreadChat: FC<ThreadChatProps> = (props: ThreadChatProps) => {
           </div>
         </div>
       ) : (
-        <div className="fixed pl-6 md:pl-16 lg:pl-44 pr-6 md:pr-16 left-0 right-0 bottom-0 w-full pb-4 bg-[#F9F9F9]">
+        <div className="fixed pl-6 md:pl-16 lg:pl-44 pr-6 md:pr-16 left-0 right-0 bottom-0 w-full pb-4 bg-[#F9F9F9] dark:bg-[#292929] dark:text-[#F9F9F9]">
           <ChatComposer
             submitDisabled={props.submitDisabled}
             messages={props.messages}
