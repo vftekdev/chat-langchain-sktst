0e7cdf4a606d1d1cb6629ad8dfe3fff5a7c69333
augustCotton123 <wco@verafiles.org>
2025-11-12 12:28:26 +0800

    test url v5

--- frontend/app/components/UrlQuestions.tsx
+++ frontend/app/components/UrlQuestions.tsx
@@ -3,10 +3,7 @@ import { useGraphContext } from "../contexts/GraphContext";
 import React, { useEffect, useRef, useState } from "react";
 
 export function UrlQuestions() {
-    const { userData } = useGraphContext();
-    const { userId } = userData;
     const threadRuntime = useThreadRuntime();
-    const [isRunning, setIsRunning] = useState(false);
 
     const handleSend = (text: string) => {
         threadRuntime.append({
@@ -22,21 +19,11 @@ export function UrlQuestions() {
         }, 400)
     }, []);
 
-    // const myDiv = document.getElementById('myDivId') as HTMLDivElement;
-    // let flag = false;
-    // if (myDiv && !flag && userId && !isRunning) {
-    //     myDiv.click();
-    //     flag = true;
-    //     setIsRunning(true);
-    // }
-
     return (
         <div
             ref={sendQuestionRef}
             onClick={() => handleSend("What is dutertes first name?")}
-            className="flex w-full bg-[#F7F9FB] dark:bg-[#1E1E1E] hover:bg-[#E5E7EB] rounded-lg shadow-lg cursor-pointer col-span-12 xl:col-span-4"
-        >
-            asdasd
-        </div>
+            className="hidden"
+        ></div>
     );
 }
