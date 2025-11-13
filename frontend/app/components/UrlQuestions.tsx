bce323c7a04c94c66567f1ebaf1cf23e41b3a1ea
VERA Files Tech Team <dev@verafiles.org>
2025-11-13 14:42:33 +0800

    fetch new file UrlQuestions.tsx to fix incorrect commit

---
+++ frontend/app/components/UrlQuestions.tsx
@@ -0,0 +1,34 @@
+import { useThreadRuntime } from "@assistant-ui/react";
+import React, { useEffect, useRef } from "react";
+import { useQueryState } from "nuqs";
+
+export function UrlQuestions() {
+    const threadRuntime = useThreadRuntime();
+    const sendQuestionRef = useRef<HTMLDivElement>(null);
+    const [uPrompt, setUPrompt] = useQueryState("uPrompt");
+
+    const handleSend = (text: string|null) => {
+        if (text) {
+            threadRuntime.append({
+                role: "user",
+                content: [{ type: "text", text }],
+            });
+        }
+    }
+
+    useEffect(() => {
+        if (uPrompt && uPrompt?.length <= 250) {
+            setTimeout(() => {
+                sendQuestionRef.current?.click()
+            }, 400);
+        }
+    }, [uPrompt]);
+
+    return (
+        <div
+            ref={sendQuestionRef}
+            onClick={() => handleSend(uPrompt)}
+            className="hidden"
+        ></div>
+    );
+}
