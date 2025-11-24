05cbe39b74d2a594bf39edd1a21f40d2dc9b3489
VERA Files Tech Team <dev@verafiles.org>
2025-11-24 10:43:07 +0800

    Revert "remove prompt and threadid on signout"
    This reverts commit 5b31cbce662970bf5f1aca34c13c64809f82e09a.

--- frontend/app/signout/SignOut.tsx
+++ frontend/app/signout/SignOut.tsx
@@ -1,13 +1,7 @@
 "use server";
 
 import { signOut } from "@/auth"
-import { useQueryState } from "nuqs";
-
 
 export default async function SignOut() {
     await signOut();
-    const [uPrompt, setUPrompt] = useQueryState("uPrompt");
-    const [threadId, setThreadId] = useQueryState("threadId");
-    setUPrompt(null);
-    setThreadId(null);
 }
