520172deac76feb0087a1e14b614e7878917f734
vfzale <zcham@verafiles.org>
2025-11-25 15:05:47 +0800

    test uprompt

--- frontend/app/components/UrlQuestions.tsx
+++ frontend/app/components/UrlQuestions.tsx
@@ -57,9 +57,13 @@ export function UrlQuestions() {
 
     return (
         <div>
+            {uPrompt ? 
             <div className="bg-black/70 flex fixed left-0 top-0 bottom-0 right-0 z-[999]">
                 <svg className="m-auto w-6 h-6 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
-            </div>
+            </div> : ""}
+            {/* <div className="bg-black/70 flex fixed left-0 top-0 bottom-0 right-0 z-[999]">
+                <svg className="m-auto w-6 h-6 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
+            </div> */}
             <div
                 ref={sendQuestionRef}
                 onClick={() => handleSend(uPrompt)}
